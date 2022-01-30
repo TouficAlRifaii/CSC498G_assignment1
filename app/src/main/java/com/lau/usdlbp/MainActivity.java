d01ea24d6291152bfa0a6d0073dc687f4e3b2c11
Toufic Al Rifaii <toufic.alrifaii@lau.edu>
2022-01-30 18:43:19 +0200

    Complete application

--- app/src/main/java/com/lau/usdlbp/MainActivity.java
+++ app/src/main/java/com/lau/usdlbp/MainActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText lbp;
     EditText usd;
     Button converter;
+    TextView converted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,39 +21,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         lbp = (EditText) findViewById(R.id.LBP);
         usd = (EditText) findViewById(R.id.USD);
+        converted = findViewById(R.id.convertedAmount);
     }
 
     public void convert(View v) {
         String lbpAmount = lbp.getText().toString();
+
         String usdAmount = usd.getText().toString();
 
-        converter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if (lbpAmount.length() == 0 && usdAmount.length() == 0) {
-                    Toast.makeText(getApplicationContext(), "Enter an Amount", Toast.LENGTH_LONG).show();
-                } else if (lbpAmount.length() > 0 && usdAmount.length() > 0) {
-                    Toast.makeText(getApplicationContext(), "Enter only 1 Amount", Toast.LENGTH_LONG).show();
-                } else if (lbpAmount.length() > 0 && usdAmount.length() == 0) {
-                    try {
-                        double lbpAm = Double.parseDouble(lbpAmount);
-                        double toUsd = lbpAm / 22000;
-                    } catch (NumberFormatException e) {
-                        Toast.makeText(getApplicationContext(), "Enter only numbers", Toast.LENGTH_LONG).show();
-                    }
-                } else {
-                    try {
-                        double usdAm = Double.parseDouble(usdAmount);
-                        double toLbp = usdAm * 22000;
-                    } catch (NumberFormatException e) {
-                        Toast.makeText(getApplicationContext(), "Enter only numbers", Toast.LENGTH_LONG).show();
-
-                    }
-                }
+        if (lbpAmount.length() == 0 && usdAmount.length() == 0) {
+            Toast.makeText(getApplicationContext(), "Enter an Amount", Toast.LENGTH_LONG).show();
+        } else if (lbpAmount.length() > 0 && usdAmount.length() > 0) {
+            Toast.makeText(getApplicationContext(), "Enter only 1 Amount", Toast.LENGTH_LONG).show();
+        } else if (lbpAmount.length() > 0 && usdAmount.length() == 0) {
+            try {
+                double lbpAm = Double.parseDouble(lbpAmount);
+                double toUsd = lbpAm / 22000;
+                converted.setText(toUsd + "$");
+            } catch (NumberFormatException e) {
+                Toast.makeText(getApplicationContext(), "Enter only numbers", Toast.LENGTH_LONG).show();
             }
-        });
+        } else {
+            try {
+                double usdAm = Double.parseDouble(usdAmount);
+                double toLbp = usdAm * 22000;
+                converted.setText(toLbp + "L.L");
+            } catch (NumberFormatException e) {
+                Toast.makeText(getApplicationContext(), "Enter only numbers", Toast.LENGTH_LONG).show();
 
-
+            }
+        }
     }
+
+
 }
